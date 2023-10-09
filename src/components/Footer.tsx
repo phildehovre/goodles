b69cd301c4fd9e0b1023ef357b62f79b28e89259
phildehovre <66724307+phildehovre@users.noreply.github.com>
2023-10-09 09:49:26 +0100

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,11 +4,21 @@ function Footer() {
   return (
     <footer className="footer-ctn">
       <ul>
-        <li>About me</li>
-        <li>Contact</li>© {new Date().getFullYear()} -{" "}
+        <li>
+          <a href="http://www.philippedehovre.com/#about" target="_blank">
+            About me
+          </a>
+        </li>
+        <li>
+          <a href="http://www.philippedehovre.com/#contact" target="_blank">
+            Contact
+          </a>
+        </li>
       </ul>
-      {new Date().getFullYear() + 1}
-      <a href="www.philippedehovre.com">Philippe De Hovre</a>
+      © {new Date().getFullYear()} - {new Date().getFullYear() + 1}
+      <a href="http://www.philippedehovre.com" target="_blank">
+        Philippe De Hovre
+      </a>
     </footer>
   );
 }
